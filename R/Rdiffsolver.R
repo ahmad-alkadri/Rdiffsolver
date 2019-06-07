58ab54444f7c5eea99171ef4a1af8905d1094504
Ahmad Alkadri <ahmad.alkadri@outlook.com>
2019-06-07 16:25:38 +0200

    reinitialized files

---
+++ R/Rdiffsolver.R
@@ -0,0 +1,16 @@
+#' Rdiffsolver: A simple package for solving diffusion equation.
+#'
+#' The \strong{Rdiffsolver} package provides currently one basic function:
+#' diff.1D, which can solve the diffusion equation in unidimensional
+#' direction, with constant boundary conditions
+#' (dirichlet and neumann with zero flux).
+#'
+#' @details \strong{Methods:} Finite differences with backward Euler
+#' implicit method (\emph{diff.1D} function).
+#'
+#' @author
+#' \strong{Maintainer:} Ahmad Alkadri (email: \href{mailto:ahmad.alkadri@outlook.com}{ahmad.alkadri@outlook.com})
+#'
+#' @docType package
+#' @name Rdiffsolver
+NULL

998f9cc872f74044fc0d49899f0416b75674658d
Ahmad Alkadri <ahmad.alkadri@umontpellier.fr>
2019-11-14 21:10:13 +0100

    Added basic implicit function

--- R/funcs_diffs_implicit.R
+++ R/funcs_diffs_implicit.R
@@ -7,11 +7,11 @@
 #' or only along one axis, b) the coefficient of diffusion is constant,
 #' and c) the boundary conditions are constant.
 #'
-#' @usage mdfexdiffu(D,dt,dx,l,T,C_i=0,C_f=1)
+#' @usage mdfimpdiffu(D,Nx,Nt,l,T,C_i=0,C_f=1)
 #'
 #' @param D Coefficient of diffusion, constant
-#' @param dt different between each time (t) steps
-#' @param dx different between each space (x) steps
+#' @param Nx number of points in space (x)
+#' @param Nt number of points in time (t)
 #' @param l half-length of the slab, usually in cm
 #' @param T Total calculated diffusion time, usually in seconds
 #' @param C_i Initial concentration value inside the slab
@@ -22,28 +22,25 @@
 #' the same C_f. If there are
 #' two elements, the first element (C_f[1]) will be on
 #' x = -l while the second one will be on x = l.
-#' @param F Fourier's mesh number, should be less than or equal to 0.5
-#' to make sure that the solution is stable
 #'
-#' @return A matrix with {round(T/dt,0)} number of row and
-#' {round(L/(sqrt(D*dt/F)),0)} number of column,
-#' profiling the diffusion on slab.
+#' @return A matrix with Nt number of row and
+#' Nx number of column, profiling the diffusion.
 #'
 #' @examples
 #' C_i = 0.00 # Initial concentration inside the slab
 #' C_f = 1.00	# Final concentration coming from outside
-#' D = 10^-7 # Coefficient of diffusion, cm^2/s
-#' dt = 60 # difference between each time step
+#' D = 1e-7 # Coefficient of diffusion, cm^2/s
+#' Nt = 100 # points in time
+#' Nx = 20 # points in space
 #' l = 0.25 # half-thickness of the slab, in cm
-#' F = 0.5 # Fourier's mesh number
 #' T = 432000 # Total measured time in seconds (~5 days)
-#' u <- mdfexdiffu(D,dt,l,T,C_i,C_f,F)
+#' u <- mdfimpdiffu(D,Nx,Nt,l,T,C_i,C_f)
 #'
 #' # Using plotly for plotting a contour plot
 #' library(plotly)
 #'
-#' df.list <- list(x = seq(-l,l,length.out = ncol(u)),
-#'                 y = seq(0,T,length.out = nrow(u)),
+#' df.list <- list(x = seq(-l,l,length.out = Nx),
+#'                 y = seq(0,T,length.out = Nt),
 #'                 z = u)
 #'
 #' plot_ly() %>%
@@ -52,45 +49,57 @@
 #'            xaxis = list(title = "x"),
 #'            yaxis = list(title = "t (s)"))
 
-mdfexdiffu <- function(D,dt,l,T,C_i=0,C_f=1,F=0.5){
+mdfimpdiffu <- function(D,Nx,Nt,l,T,C_i=0,C_f=1){
 
   if(length(C_f) > 2){
     stop("ERROR: C_f must consist of maximum 2 elements")
   }
 
   L = 2*l
-  Nt = round(T/dt,0)
   # Mesh point in time
-  t = seq(0, Nt*dt, length.out=Nt)
-  dx = sqrt(D*dt/F)
-  Nx = round(L/dx,0)
+  t = seq(0, T, length.out=Nt)
   # Mesh point in space
   x = seq(0,L,length.out=Nx)
-  # Verify
+  # Step size for space (dx)
   dx = x[2]-x[1]
+  # Step size for space (dt)
   dt = t[2]-t[1]
+  # F
+  F = D*dt/(dx^2)
+  
+  # Structures for the linear system
+  A = matrix(0, nrow = Nx+1, ncol = Nx+1)
 
-  # Matrice vide pour u et u_n
-  u = matrix(0,ncol=Nx,nrow=Nt)
-  u_n	= seq(0,0,length.out=Nx)
-
-  # Condition initiale
-  for(i in 1:(Nx+1)){
-    u_n[i] = C_i
+  for(i in 2:Nx){
+     A[i,i-1] = -F
+     A[i,i+1] = -F
+     A[i,i] = 1+2*F
   }
+
+  # Boundary condition
+  A[1,1] = 1
+  A[Nx+1,Nx+1] = 1
+
+  # Empty matrix for u and u_n
+  u = matrix(0, nrow = Nx+1, ncol = Nt+1)
+  # Boundary condition on the borders
+  u[1,] = C_f[1]
+  u[Nx+1,] = C_f[length(C_f)]
+  # Initial condition
+  u[2:Nx,] = C_i
 
   # Compute
-  for(n in 1:Nt){
-    # Compute u at mesh points
-    for(i in 2:Nx){
-      u[n,i] = u_n[i]+F*(u_n[i-1]-2*u_n[i]+u_n[i+1])
-    }
-    # Insert boundary conditions
-    u[n,1] = C_f[1] ; u[n,Nx] = C_f[length(C_f)]
-
-    # Update u_n before next step
-    u_n = u[n,]
+  for(n in 2:Nt+1){
+     u_n = u[,n-1]
+     b = matrix(0, nrow = Nx+1, ncol = 1)
+     b[2:Nx] = u_n[2:Nx]
+     b[1] = C_f
+     b[Nx+1] = C_f[length(C_f)]
+     ui = solve(A,b)
+     u[,n] = ui
   }
+  
+  u = t(u)
 
   return(u)
 
